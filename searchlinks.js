1a41294fa9066abdf9c6239bf3deeae2385e93dc
星海 <69918945+lovelyCARDINAL@users.noreply.github.com>
2021-11-16 23:42:27 +0800

    Update searchlinks.js
    支持繁體介面

--- searchlinks.js
+++ searchlinks.js
@@ -6,7 +6,7 @@ if ( NS.includes(mw.config.get('wgNamespaceNumber')) ) {
       mw.config.get('wgScript') + "?title=Special:Search&search=" + "insource:%22" +
       (mw.config.get('wgTitle').split(" ").join("_")) + "%22" + "+linksto:%22" + 
       (mw.config.get('wgPageName').split(" ").join("_")) + "%22" + "&profile=advanced&ns0=1&ns4=1&ns8=1&ns10=1&ns12=1&ns14=1&ns828=1", 
-    '查找链入', 'ca-searchlinks', "通过高级搜索查找链入");
+    wgULS('查找链入', '搜尋連入'), 'ca-searchlinks', wgULS('通过高级搜索查找链入', '搜尋實際連結至此的頁面'));
   });
 }
 if (mw.config.get('wgNamespaceNumber') === 10) {
@@ -16,6 +16,6 @@ if (mw.config.get('wgNamespaceNumber') === 10) {
       mw.config.get('wgScript') + "?title=Special:Search&search=" + "insource:%22" +
       (mw.config.get('wgTitle').split(" ").join("_")) + "%22" + "+hastemplate:%22" + 
       (mw.config.get('wgTitle').split(" ").join("_")) + "%22" + "&profile=advanced&ns0=1&ns4=1&ns8=1&ns10=1&ns12=1&ns14=1&ns828=1", 
-    '查找嵌入', 'ca-searchlinks', "通过高级搜索查找嵌入");
+    wgULS('查找嵌入', '搜尋嵌入'), 'ca-searchlinks', wgULS('通过高级搜索查找嵌入', '搜尋實際的嵌入'));
   });
 }
